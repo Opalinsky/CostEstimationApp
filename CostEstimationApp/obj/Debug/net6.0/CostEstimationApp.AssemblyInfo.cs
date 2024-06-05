60d4f166426c5666986961be127286db376052e2
Opalinsky <feelipemm22@gmail.com>
2024-06-05 03:19:04 +0200

    Zacznij od dodania klas dokladnosci i chropowatosci

--- CostEstimationApp/obj/Debug/net6.0/CostEstimationApp.AssemblyInfo.cs
+++ CostEstimationApp/obj/Debug/net6.0/CostEstimationApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CostEstimationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ffe8bde1e67121eb7930d762fe64da2d8bb977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17ea35a77b6d8762eb2fcf7cd42844373e8a62b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CostEstimationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CostEstimationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
